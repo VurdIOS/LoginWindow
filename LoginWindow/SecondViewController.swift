cd81a75d3a5e4e6afd35e074044ec99a3b527d45
Kamal <kamalios@mail.ru>
2023-02-13 22:10:42 +0300

    Setup App Did first screen and all elements on it Did second screen and all elements on it

---
+++ LoginWindow/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LoginWindow
+//
+//  Created by Камаль Атавалиев on 13.02.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+    
+
+}
